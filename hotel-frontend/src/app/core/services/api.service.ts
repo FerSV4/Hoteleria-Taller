bf9b3bf1484f11981232d214d87de9e5e054a0ff
FerSV4 <ferandressc@hotmail.com>
2026-06-11 20:58:42 -0400

    fix(frontend): Mod. de la api, ahora apunta a Railway

--- hotel-frontend/src/app/core/services/api.service.ts
+++ hotel-frontend/src/app/core/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'http://localhost:3000/api';
+  private baseUrl = 'https://hoteleria-taller-production.up.railway.app/api';
 
   constructor(private http: HttpClient) { }
 
