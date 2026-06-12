c098e785ad425593cf146d4dadc5a43a6b7b639a
FerSV4 <ferandressc@hotmail.com>
2026-06-12 11:51:17 -0400

    refactor: [HU-01] mejora de la sentencia de supa y la funcion mas eficiente

--- hotel-backend/src/services/huespedService.js
+++ hotel-backend/src/services/huespedService.js
@@ -1,10 +1,13 @@
 const prisma = require('../config/db');
 
 const registrarHuesped = async (datosHuesped) => {
+    //En este refactor mejoramos la funcion  del registro, trayendo solo el id, para verif si existe o no.
     const huespedExistente = await prisma.huesped.findUnique({
-        where: { documento: datosHuesped.documento }
+        where: { documento: datosHuesped.documento },
+        select: { id: true }
     });
 
+    //funcion de verificacion de duplicador, despues del retro...tdd
     if (huespedExistente) {
         throw new Error('DUPLICADO');
     }
